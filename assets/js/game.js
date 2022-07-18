b388c68b1b41191d827649f64414ebf884bb12c0
Sammy <marchsam1988@gmail.com>
2022-07-18 22:26:14 +0930

    added some id's to the questionsindex and started some basic functions for the questions and score tally

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,10 +2,57 @@ var question = document.getElementById("question");
 var choiceA = document.getElementById("A");
 var choiceB = document.getElementById("B");
 
+var finalScore = [];
+
 function question1() {
     question.innerHTML = "Would you break the law to save someone?";
     choiceA.innerHTML = "";
     choiceB.innerHTML = "";
 }
-  
+
+function question2() {
+    question.innerHTML = "Would you break the law to save someone?";
+    choiceA.innerHTML = "";
+    choiceB.innerHTML = "";
+}
+
+function question3() {
+    question.innerHTML = "Would you break the law to save someone?";
+    choiceA.innerHTML = "";
+    choiceB.innerHTML = "";
+}
+
+function question4() {
+    question.innerHTML = "Would you break the law to save someone?";
+    choiceA.innerHTML = "";
+    choiceB.innerHTML = "";
+}
+
+function question5() {
+    question.innerHTML = "Would you break the law to save someone?";
+    choiceA.innerHTML = "";
+    choiceB.innerHTML = "";
+}
+
+function question6() {
+    question.innerHTML = "Would you break the law to save someone?";
+    choiceA.innerHTML = "";
+    choiceB.innerHTML = "";
+}
+
+function question7() {
+    question.innerHTML = "Would you break the law to save someone?";
+    choiceA.innerHTML = "";
+    choiceB.innerHTML = "";
+}
+
+choiceA.addEventListener("click", function() {
+    finalScore.push(0);
+});
+
+choiceB.addEventListener("click", function() {
+    finalScore.push(1);
+});
+
+
 question1();
