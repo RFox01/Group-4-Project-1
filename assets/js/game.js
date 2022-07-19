26d6224d77eea5422c2ef28f9c7d1f4e31cc2bb2
Sammy <marchsam1988@gmail.com>
2022-07-19 10:55:47 +0930

    added small changes like id's to elements, changed a div to button and tried to implement the core algorythm, however it still needs work

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,18 +6,18 @@ var finalScore = [];
 
 function question1() {
     question.innerHTML = "Would you break the law to save someone?";
-    choiceA.innerHTML = "";
-    choiceB.innerHTML = "";
+    choiceA.innerHTML = "<img src=https://bulma.io/images/placeholders/1280x960.png>";
+    choiceB.innerHTML = "<img src=https://bulma.io/images/placeholders/1280x960.png>";
 }
 
 function question2() {
-    question.innerHTML = "Would you break the law to save someone?";
+    question.innerHTML = "Flight or Super Strength?";
     choiceA.innerHTML = "";
     choiceB.innerHTML = "";
 }
 
 function question3() {
-    question.innerHTML = "Would you break the law to save someone?";
+    question.innerHTML = "Speed or Power?";
     choiceA.innerHTML = "";
     choiceB.innerHTML = "";
 }
@@ -46,29 +46,37 @@ function question7() {
     choiceB.innerHTML = "";
 }
 
+/* Adding an event listener to the choiceA button. When the button is clicked, it will push a 0 to the
+finalScore array and call the nextQuestion function. */
 choiceA.addEventListener("click", function() {
-    finalScore.push(0);
+    finalScore.push('0');
     nextQuestion();
 });
 
 choiceB.addEventListener("click", function() {
-    finalScore.push(1);
+    finalScore.push('1');
     nextQuestion();
 });
 
 question1();
 
+/**
+ * If the finalScore array is equal to a certain value, then run a certain function.
+ */
 function nextQuestion() {
-if (finalScore === [0]) {
+if (finalScore === ['0']) {
     question2();
-} else if (finalScore === [1]) {
+} else if (finalScore === ['1']) {
     question3();
-} else if (finalScore === [0, 0]) {
+} else if (finalScore === ['0', '0']) {
     question4();
-} else if (finalScore === [0, 1]) {
+} else if (finalScore === ['0', '1']) {
     question5();
-} else if (finalScore === [1, 0]) {
+} else if (finalScore === ['1', '0']) {
     question6();
-} else if (finalScore === [1, 1]) {
+} else if (finalScore === ['1', '1']) {
     question7();
 }};
+
+
+console.log(finalScore.length);
