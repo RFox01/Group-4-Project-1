43bcbee65d17c99a7bc45a65581d5128f7d2d353
Sammy <marchsam1988@gmail.com>
2022-07-20 16:36:08 +0930

    added end page javascript and finalised minmap with questions html questions and answers

---
+++ assets/js/end.js
@@ -0,0 +1,14 @@
+// retrieve data from finalScore
+var finalScore = localStorage.getItem("finalScore");
+var stringValue = '';
+
+//examples
+function CaptainAmerica() {
+    this.img = "https://bulma.io/images/placeholders/1280x960.png";
+    stringValue = '011';
+}
+
+function Thor() {
+    this.img = "https://bulma.io/images/placeholders/1280x960.png";
+    stringValue = '010';
+}
