a19ee14673101e7c560e5bd9355458464d9d828f
Appoline Cogan <appolinecogan@gmail.com>
2022-07-23 17:46:22 +0700

    updated all html with toggable burger menu and fixed hovering issues + little styles issues

--- assets/js/game.js
+++ assets/js/game.js
@@ -130,3 +130,19 @@ function nextQuestion() {
 }
 
 console.log(finalScore);
+
+// this is to make sure the hamburger menu is clickable
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Get  "navbar-burger" elements
+  const navbarBurgers = document.querySelector(".navbar-burger");
+
+  const navBarMenu = document.querySelector(".navbar-menu");
+  // Add a click event on each of them
+  navbarBurgers.addEventListener("click", () => {
+    
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    navbarBurgers.classList.toggle("is-active");
+    navBarMenu.classList.toggle("is-active");
+  });
+});
